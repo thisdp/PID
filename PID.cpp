cd5b8a03a0b3149fe0403dd058d8535708b7f569
thisdp <30386052+thisdp@users.noreply.github.com>
2021-09-15 13:32:51 +0800

    Update PID.cpp

--- PID.cpp
+++ PID.cpp
@@ -30,11 +30,11 @@ void PID::update() {
   double inputTransformed = hardcodeMap(input, inputScalerMin, inputScalerMax, -1.0, 1.0);
   e0 = targetTransformed-inputTransformed;
 	double kp = KpFunction == 0 ? Kp : KpFunction(e0, Kp), kd = KdFunction == 0 ? Kd : KdFunction(e0, Kd), ki = KiFunction == 0 ? Ki : KiFunction(e0, Ki);
-	double a2 = kd / sT;
-	double a1 = kp + 2.0 * a2;
-	double a0 = kp + ki * sT + a2;
+	double ap = (e0-e1)*kp;
+  double ai = e1*ki;
+  double ad = (e0-2*e1+e2)*kd;
 	o1 = o0;
-	o0 = o1 + a0 * e0 + a1 * e1 + a2 * e2;
+	o0 = o1+ap+ai+ad;
 	if (o0 > 1) o0 = 1;
 	if (o0 < -1) o0 = -1;
 	output = hardcodeMap(o0, -1.0, 1.0, outputScalerMin, outputScalerMax);
